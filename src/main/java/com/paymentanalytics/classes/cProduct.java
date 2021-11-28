6a66e2c51d12d5c1ff4fa40c4df212e9e0adc924
Roger-Ramos-Sabadell <rogerramoss98@gmail.com>
2021-11-28 23:56:21 +0100

    Entrega final

--- src/main/java/com/paymentanalytics/classes/cProduct.java
+++ src/main/java/com/paymentanalytics/classes/cProduct.java
@@ -7,40 +7,8 @@ import java.util.UUID;
 @Entity
 public class cProduct {
     @Id
-    private UUID _id;
-    private String name;
-    private double price;
-    private String description;
-
-    public UUID get_id() {
-        return _id;
-    }
-
-    public void set_id(UUID _id) {
-        this._id = _id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    UUID _id;
+    String name;
+    double price;
+    String description;
 }
--- src/main/java/com/paymentanalytics/classes/cTicket.java
+++ src/main/java/com/paymentanalytics/classes/cTicket.java
@@ -10,40 +10,8 @@ import java.util.UUID;
 public class cTicket {
 
     @Id
-    private UUID _id;
-    private String productId;
-    private double amount;
-    private ePaymentType paymentType;
-
-    public UUID get_id() {
-        return _id;
-    }
-
-    public void set_id(UUID _id) {
-        this._id = _id;
-    }
-
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public ePaymentType getPaymentType() {
-        return paymentType;
-    }
-
-    public void setPaymentType(ePaymentType paymentType) {
-        this.paymentType = paymentType;
-    }
+    UUID _id;
+    String productId;
+    double amount;
+    ePaymentType paymentType;
 }
